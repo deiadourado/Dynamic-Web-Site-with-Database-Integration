733aded3ef52e1b8a7f84c1c44597611e0f1e4a8
Andreia Dourado <deiadourado93@gmail.com>
2023-10-11 18:19:50 -0400

    update view.php

---
+++ view.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Geek's Gossip</title>
+</head>
+
+<body>
+    <header>
+        <div align="center">
+            <h1>Geek's Gossip</h1>
+        </div>
+        <h2>Your geek news portal!</h2>
+    </header>
+
+    <main class="container">
+        <div class="table">
+            <table class="table">
+                <tr>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Email</th>
+                    <th>Birthday</th>
+                    <th>Topics</th>
+                </tr>
+
+                <?php
+        $conn = mysqli_connect("localhost", "root", "", "geekdb"); //database file
+
+//check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT fname, lname, email, birthday, checkbox FROM geekperson";
+    
+    if ($conn->query($sql) === TRUE) {
+        echo "Your data has been saved successfully!";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+} else {
+    echo "Invalid request"; // Handle non-POST requests
+}
+
+// close the connection
+$conn->close();
+?>
+            </table>
+        </div>
+      </main>
+
+    <footer>
+        Made by Andreia Dourado (2023)
+    </footer>
+</body>
+
+</html>
